fa9dde4b97e1556e48c53acf70368f0e670d9d90
AbhisKmr <akaks09@gmail.com>
2025-03-14 17:20:08 +0530

    update: test activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("com.google.firebase:firebase-crashlytics:19.4.1")
+    implementation("androidx.activity:activity:1.10.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
